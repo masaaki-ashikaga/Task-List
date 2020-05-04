d8cd5ca1f9e40d12644ef1d8210d11950b39e7a0
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-05-05 01:06:14 +0900

    プロジェクトタスクのレイアウト作成

---
+++ project_task.php
@@ -0,0 +1,11 @@
+<?php
+require_once('config.php');
+require_once('./helpers/db_helper.php');
+require_once('./helpers/common_helper.php');
+
+//データベース接続
+
+
+include_once('./views/project_task_view.php');
+
+?>
---
+++ views/project_task_view.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/base.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>タスク管理アプリ</title>
+</head>
+
+<body>
+    <main>
+        <h1 class="page-title">プロジェクトタスク</h1>
+        <p class="form-title">XXX会社WEBシステム</p>
+
+        <div class="my-project">
+            <div class="task-label">
+                <p class="label">タスク名</p>
+                <p class="label">完了チェック</p>
+            </div>
+            <div class="task-list">
+                <div class="task-info">
+                    <p class="task-name">デザインコーディング</p>
+                    <p class="task-deadline">期限：2019/3/31</p>
+                </div>
+                <p class="task-member">糸島｜高橋</p>
+                <label class="task-checkbox"><input type="checkbox" class="task-complete"><span class="checkbox"></label>
+            </div>
+            <div class="delete-btn"><p class="task-delete">削除</p></div>
+
+            <div class="task-list">
+                <div class="task-info">
+                    <p class="task-name">デザインコーディング</p>
+                    <p class="task-deadline">期限：2019/3/31</p>
+                </div>
+                <p class="task-member">糸島｜高橋</p>
+                <label class="task-checkbox"><input type="checkbox" class="task-complete"><span class="checkbox"></label>
+            </div>
+            <div class="delete-btn"><p class="task-delete">削除</p></div>
+
+            <div class="task-list">
+                <div class="task-info">
+                    <p class="task-name">デザインコーディング</p>
+                    <p class="task-deadline">期限：2019/3/31</p>
+                </div>
+                <p class="task-member">糸島｜高橋</p>
+                <label class="task-checkbox"><input type="checkbox" class="task-complete"><span class="checkbox"></label>
+            </div>
+            <div class="delete-btn"><p class="task-delete">削除</p></div>
+        </div>
+
+        <div class="account">
+            <p class="form-title">アカウント情報</p>
+            <div class="account-info">
+                <form class="account-info-form" action="./dashboard.php" method="POST">
+                    <p><label for="name">名前</label></p>
+                    <p style='color: red; font-size: 13px'><?php if(!empty($errs['name'])){ echo $errs['name']; } ?></p>
+                    <p><input type="text" name="name" class="name"></p>
+                    <p><label for="mail">メール</label></p>
+                    <p style='color: red; font-size: 13px'><?php if(!empty($errs['mail'])){ echo $errs['mail']; } ?></p>
+                    <p><input type="text" name="mail" class="mail"></p>
+                    <p><label for="comment">一言</label></p>
+                    <p style='color: red; font-size: 13px'><?php if(!empty($errs['comment'])){ echo $errs['comment']; } ?></p>
+                    <p><input type="text" name="comment" class="comment"></p>
+                    <p class="btn"><input type="submit" name="account" class="login-btn" value="変更する"></p>
+                </form>
+            </div>
+        </div>
+
+        <div class="new-project">
+            <p class="form-title">プロジェクトを作る</p>
+            <div class="project-make">
+                <form class="project-make-form" action="./dashboard.php" method="POST">
+                    <p><label for="pj_name">プロジェクト名</label></p>
+                    <p style='color: red; font-size: 13px'><?php if(!empty($errs['pj_name'])){ echo $errs['pj_name']; } ?></p>
+                    <p><input type="text" name="pj_name" class="name"></p>
+                    <p><label for="explain">説明</label></p>
+                    <p style='color: red; font-size: 13px'><?php if(!empty($errs['pj_explain'])){ echo $errs['pj_explain']; } ?></p>
+                    <p><textarea name="pj_explain" class="explain" cols="42" rows="5"></textarea></p>
+                    <p class="btn"><input type="submit" name="project" class="login-btn" value="作成する"></p>
+                </form>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
